318fbbd1ec270f751e651c66b87bf155af6900a2
fzhmghfrh <faizahmaghfirah@gmail.com>
2024-09-05 18:41:51 +0700

    remove: unused lines

--- domain/repository/userRepository.js
+++ domain/repository/userRepository.js
@@ -1,34 +1,3 @@
-// const User = require('../model/userModel');
-
-// // Add a new user
-// const addUser = async (user) => {
-//     const newUser = new User(user);
-//     return await newUser.save();
-// };
-
-// // Get a user by ID
-// const getUserByUserId = async (userId) => {
-//     return await User.findOne({ user_id: userId });
-// };
-
-// // Get all users
-// const getAllUsers = async () => {
-//     return await User.find();
-// };
-
-// // Delete a user by ID
-// const deleteUserById = async (userId) => {
-//     return await User.findOneAndDelete({ user_id: userId });
-// };
-
-// module.exports = {
-//     addUser,
-//     getUserByUserId,
-//     getAllUsers,
-//     deleteUserById
-// };
-
-
 const User = require('../model/userModel');
 
 // Add a new user
