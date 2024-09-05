578370c70ed011cc744726d2ec1170498e6e2587
fzhmghfrh <faizahmaghfirah@gmail.com>
2024-09-05 18:39:25 +0700

    fix: read, update, delete

--- domain/repository/userRepository.js
+++ domain/repository/userRepository.js
@@ -1,12 +1,43 @@
+// const User = require('../model/userModel');
+
+// // Add a new user
+// const addUser = async (user) => {
+//     const newUser = new User(user);
+//     return await newUser.save();
+// };
+
+// // Get a user by ID
+// const getUserByUserId = async (userId) => {
+//     return await User.findOne({ user_id: userId });
+// };
+
+// // Get all users
+// const getAllUsers = async () => {
+//     return await User.find();
+// };
+
+// // Delete a user by ID
+// const deleteUserById = async (userId) => {
+//     return await User.findOneAndDelete({ user_id: userId });
+// };
+
+// module.exports = {
+//     addUser,
+//     getUserByUserId,
+//     getAllUsers,
+//     deleteUserById
+// };
+
+
 const User = require('../model/userModel');
 
 // Add a new user
-const addUser = async (user) => {
-    const newUser = new User(user);
-    return await newUser.save();
+const addUser = async (userData) => {
+    const user = new User(userData);
+    return await user.save();
 };
 
-// Get a user by ID
+// Get a user by user_id
 const getUserByUserId = async (userId) => {
     return await User.findOne({ user_id: userId });
 };
@@ -16,9 +47,9 @@ const getAllUsers = async () => {
     return await User.find();
 };
 
-// Delete a user by ID
-const deleteUserById = async (id) => {
-    return await User.findOneAndDelete({ id });
+// Delete a user by user_id
+const deleteUserById = async (userId) => {
+    return await User.deleteOne({ user_id: userId });
 };
 
 module.exports = {
--- domain/usecase/userUsecase.js
+++ domain/usecase/userUsecase.js
@@ -1,3 +1,92 @@
+// const bcrypt = require('bcrypt');
+// const User = require('../model/userModel');
+// const userRepository = require('../repository/userRepository');
+// const { v4: uuidv4 } = require('uuid');
+
+// const SALT_ROUNDS = 10; // Number of salt rounds for bcrypt
+
+// const generateUserId = () => {
+//     return uuidv4();
+// };
+
+// // Function to create a new user
+// const createUser = async ({ username, email, password, role }) => {
+//     try {
+//         const user_id = generateUserId();
+//         // Hash the password using bcrypt
+//         const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+//         // Create a new user using the Mongoose model
+//         const newUser = new User({
+//             user_id,
+//             username,
+//             email,
+//             password: hashedPassword,
+//             role,
+//         });
+
+//         // Save the new user to the database
+//         await newUser.save();
+
+//         return newUser;
+//     } catch (error) {
+//         throw new Error('Error creating user: ' + error.message);
+//     }
+// };
+// // const getUser = async (id) => {
+// //     return await userRepository.getUserById(id);
+// // };
+
+// // const getAllUsers = async () => {
+// //     return await userRepository.getAllUsers();
+// // };
+
+// const getAllUsers = async () => {
+//     try {
+//         const users = await User.find({});
+//         console.log('Retrieved users:', users);
+//         return users;
+//     } catch (error) {
+//         console.error('Error retrieving users:', error);
+//         throw error;
+//     }
+// };
+
+// const getUser = async (userId) => {
+//     try {
+//         // const user = await User.findById(userId);
+//         // console.log('Retrieved user:', user);
+//         // return user;
+//         return await userRepository.getUserByUserId(userId);
+//     } catch (error) {
+//         console.error('Error retrieving user:', error);
+//         throw error;
+//     }
+// };
+
+
+// const updateUser = async (id, { name, password }) => {
+//     const user = await userRepository.getUserById(id);
+//     if (!user) {
+//         return null;
+//     }
+//     user.name = name;
+//     user.password = password;
+//     return await user.save();
+// };
+
+// const deleteUser = async (id) => {
+//     return await userRepository.deleteUserById(id);
+// };
+
+// module.exports = {
+//     createUser,
+//     getUser,
+//     getAllUsers,
+//     updateUser,
+//     deleteUser
+// };
+
 const bcrypt = require('bcrypt');
 const User = require('../model/userModel');
 const userRepository = require('../repository/userRepository');
@@ -10,73 +99,34 @@ const generateUserId = () => {
 };
 
 // Function to create a new user
-const createUser = async ({ username, email, password, role }) => {
-    try {
-        const user_id = generateUserId();
-        // Hash the password using bcrypt
-        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-
-        // Create a new user using the Mongoose model
-        const newUser = new User({
-            user_id,
-            username,
-            email,
-            password: hashedPassword,
-            role,
-        });
-
-        // Save the new user to the database
-        await newUser.save();
-
-        return newUser;
-    } catch (error) {
-        throw new Error('Error creating user: ' + error.message);
-    }
+const createUser = async (userData) => {
+    userData.user_id = generateUserId();
+    userData.password = await bcrypt.hash(userData.password, SALT_ROUNDS);
+    return await userRepository.addUser(userData);
 };
-// const getUser = async (id) => {
-//     return await userRepository.getUserById(id);
-// };
 
-// const getAllUsers = async () => {
-//     return await userRepository.getAllUsers();
-// };
-
+// Function to get all users
 const getAllUsers = async () => {
-    try {
-        const users = await User.find({});
-        console.log('Retrieved users:', users);
-        return users;
-    } catch (error) {
-        console.error('Error retrieving users:', error);
-        throw error;
-    }
+    return await userRepository.getAllUsers();
 };
 
+// Function to get one user by user_id
 const getUser = async (userId) => {
-    try {
-        // const user = await User.findById(userId);
-        // console.log('Retrieved user:', user);
-        // return user;
-        return await userRepository.getUserByUserId(userId);
-    } catch (error) {
-        console.error('Error retrieving user:', error);
-        throw error;
-    }
+    return await userRepository.getUserByUserId(userId);
 };
 
-
-const updateUser = async (id, { name, password }) => {
-    const user = await userRepository.getUserById(id);
-    if (!user) {
-        return null;
+// Function to update user by user_id
+const updateUser = async (userId, userData) => {
+    if (userData.password) {
+        userData.password = await bcrypt.hash(userData.password, SALT_ROUNDS);
     }
-    user.name = name;
-    user.password = password;
-    return await user.save();
+    userData.updated_at = Date.now();
+    return await User.findOneAndUpdate({ user_id: userId }, userData, { new: true });
 };
 
-const deleteUser = async (id) => {
-    return await userRepository.deleteUserById(id);
+// Function to delete user by user_id
+const deleteUser = async (userId) => {
+    return await userRepository.deleteUserById(userId);
 };
 
 module.exports = {
